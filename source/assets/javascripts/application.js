2a075498c4c425785fa3790dd1689f00fd69fdb0
Eric Gardner <gardner.ec@gmail.com>
2016-07-14 14:36:33 -0700

    Add basic Smoothstate transitions
    This adds in a basic SmoothState flow, using some transitions from
    velocity.js to choreograph between pages.

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -2,4 +2,23 @@
 
 $(document).ready(function(){
   uiSetup();
+
+  $("#main").smoothState({
+    onStart: {
+      duration: 400,
+      render: function($container) {
+        $container.velocity('fadeOut', { duration: 200 });
+      }
+    },
+    onReady: {
+      duration: 400,
+      render: function($container, $newContent) {
+        $container.html($newContent);
+        $container.velocity('fadeIn', { duration: 100 });
+      }
+    },
+    onAfter: function($container, $newContent) {
+      uiSetup();
+    }
+  })
 });
